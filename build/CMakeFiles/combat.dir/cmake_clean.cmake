4fbcf18d290d229948c9c51523d29f77928c16a3
manith-hetti <76125387+manith-hetti@users.noreply.github.com>
2024-08-17 13:11:53 +0100

    added Cmake file

---
+++ build/CMakeFiles/combat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/combat.dir/main.cpp.o"
+  "CMakeFiles/combat.dir/main.cpp.o.d"
+  "combat"
+  "combat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/combat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
